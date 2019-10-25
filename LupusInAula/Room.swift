1cafcb402ce55bf91ff0041813efc9a01a46a456
Il_Mil1 <andrea.milone98@gmail.com>
2019-10-25 12:56:14 +0200

    Creazione room

---
+++ LupusInAula/Room.swift
@@ -0,0 +1,17 @@
+//
+//  Room.swift
+//  LupusInAula
+//
+//  Created by Andrea Milone on 25/10/2019.
+//  Copyright © 2019 ITS. All rights reserved.
+//
+
+import Foundation
+
+
+
+// Struttura tipo player
+// Funzione su giorno e notte
+// Remove from the room
+// Funzione della ricezione delle persone.
+
