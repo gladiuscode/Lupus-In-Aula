270d4040b8b62a6ee876504b870f758b6bd02962
Riccardo Scambia <riccardoscambia@gmail.com>
2019-10-25 11:50:54 +0200

    progress in unit testing

--- LupusInAulaTests/LupusInAulaTests.swift
+++ LupusInAulaTests/LupusInAulaTests.swift
@@ -11,6 +11,23 @@ import XCTest
 
 class LupusInAulaTests: XCTestCase {
 
+    func testAssignRoleLogic_arrayOfPlayersIsEmpty_shouldReturnEmptyArray() {
+        
+        let result = assignPlayersRole(withPlayers: [Playing]())
+        
+        XCTAssertTrue(result.isEmpty)
+        XCTAssertEqual(result.count, 0)
+        
+    }
+    
+    func testAssignRoleLogic_arrayOfPlayersWithOnePlayer_shouldReturnEmptyArray() {
+        
+        let player = Player(nickname: "test", roleType: .cheater, isAlive: true, isHost: nil, vote: nil)
+        
+        let result = assignPlayersRole(withPlayers: [player])
+        XCTAssertTrue(result.isEmpty)
+        
+    }
     
 
 }
